1f6127477624e65eefb7820cf53929f17ce295a9
telugukalituts <70319225+telugukalituts@users.noreply.github.com>
2021-08-21 10:02:57 +0530

    Add files via upload

---
+++ Bank/index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'>
+    <link rel="stylesheet" type="text/css" href="css/style1.css">
+
+    <meta name="theme-color" content="#3a9fff"><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  <!-- partial:index.partial.html -->
+
+
+<!-- Header -->
+
+
+<!-- Container -->
+<div class="container">
+  <div class="content">
+    <h1>Digital Agency That Helps You To Go Ahead</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione blanditiis odio aperiam. Quas, alias nam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, illo.</p>
+    <a href="#">Learn More</a>
+  </div>
+  <div class="bg-image">
+    <!-- <img src="https://i.ibb.co/Db6LXYV/back.png" alt=""> -->
+   
+    <img src=" img/1.svg" alt="">
+  </div>
+</div>
+
+<!-- Footer -->
+<div class="ftr">
+  <p> © 2021 - Spark Bank</p>
+  <p>Developed by Chetan Vivek</p>
+</div>
+<!-- partial -->
+      <!-- <div class="container-fluid"> -->
+      <!-- Introduction section -->
+            <!-- <div class="row intro py-1" style="background-color : lightblue ;">
+              
+              <div class="col-sm-12 col-md img text-center">
+                <img src="img/bank.jpg" class="img-fluid pt-2">
+              </div>
+            </div> -->
+              
+      <!-- Activity section -->
+            <!-- <div class="row activity text-center">
+                  <div class="col-md act">
+                    <img src="img/customer.jpg" class="img-fluid">
+                    <br>
+                    <a href="createuser.php"><button style="background-color : #2785C4;" style="border-radius:0%">Create a User</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/aboutus.jpg" class="img-fluid">
+                    <br>
+                    <a href="removeuser.php"><button style="background-color : #2785C4;" style="border-radius:0%">Delete Users</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/transfermoney.jpg" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button style="background-color : #2785C4;">Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/transhistory.png" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button style="background-color : #2785C4;">Transaction History</button></a>
+                  </div>
+                  
+            </div>
+      </div> -->
+
+      <!-- Footer -->
+
+
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Bank/navbar.php
@@ -0,0 +1,40 @@
+<!-- navbar --> 
+      <!-- <nav class="navbar navbar-expand-md navbar-dark bg-light">
+      <a class="navbar-brand" href="index.php" style="color:black"><img src="img/tsf logo.png" alt="Logo" width="85" style="margin-bottom:4px;"> <b>Spark Foundation Bank</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : black;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : black;"><b>Create a User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removeuser.php" style="color : black;"><b>Remove user</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : black;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : black;"><b>Transaction history</b></a>
+              </li>
+          </div>
+       </nav> -->
+       <header class="hdr">
+  <input type="checkbox" id="menu-dis">
+  <label for="menu-dis"><i class="fas fa-bars"></i></label>
+  <label for="menu-dis"><i class="fas fa-times"></i></label>
+  <div class="logo">
+    <h3>Spark<span>Bank</span></h3>
+  </div>
+  <ul class="menu">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="createuser.php">Create a User</a></li>
+    <li><a href="removeuser.php">Remove user</a></li>
+    <li><a href="transfermoney.php">Transfer Money</a></li>
+    <li><a class="get-btn" href="createuser.php">Get Started</a></li>
+  </ul>
+</header>
