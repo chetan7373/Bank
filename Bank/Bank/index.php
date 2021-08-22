a02a86abaeb2efe6755538dc51c33baa59e180ea
chetan7373 <70319225+chetan7373@users.noreply.github.com>
2021-08-22 11:08:00 +0530

    Add files via upload

---
+++ Bank/Bank/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'>
+    <link rel="stylesheet" type="text/css" href="css/style1.css">
+
+    <meta name="theme-color" content="#3a9fff"><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  <!-- partial:index.partial.html -->
+
+
+<!-- Header -->
+
+
+<!-- Container -->
+<div class="container">
+  <div class="content">
+    <h1>Digital Agency That Helps You To Go Ahead</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione blanditiis odio aperiam. Quas, alias nam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, illo.</p>
+    <a href="#">Learn More</a>
+  </div>
+  <div class="bg-image">
+
+   
+    <img src=" img/1.svg" alt="">
+  </div>
+</div>
+
+<!-- Footer -->
+<div class="ftr">
+  <p> © 2021 - Spark Bank</p>
+  <p>Developed by Chetan Vivek</p>
+</div>
+
+
+
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Bank/Bank/navbar.php
@@ -0,0 +1,16 @@
+
+       <header class="hdr">
+  <input type="checkbox" id="menu-dis">
+  <label for="menu-dis"><i class="fas fa-bars"></i></label>
+  <label for="menu-dis"><i class="fas fa-times"></i></label>
+  <div class="logo">
+    <h3>Spark<span>Bank</span></h3>
+  </div>
+  <ul class="menu">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="createuser.php">Create a User</a></li>
+    <li><a href="removeuser.php">Remove user</a></li>
+    <li><a href="transfermoney.php">Transfer Money</a></li>
+    <li><a class="get-btn" href="createuser.php">Get Started</a></li>
+  </ul>
+</header>
